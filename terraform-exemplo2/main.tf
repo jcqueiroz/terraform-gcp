506ba4a9e2a6d75505f78657b42058fdf5b49791
Jean Queiroz <jean@jcqueiroz.dev>
2022-12-28 18:09:34 +0000

    change-departments-tags-02

--- terraform-exemplo2/main.tf
+++ terraform-exemplo2/main.tf
@@ -28,7 +28,7 @@ resource "google_compute_instance" "vm_instance" {
   tags = ["prod"]
 
   labels = {
-    centro_custo = "${var.ceoperations-it}"
+    centro_custo = "${var.centro_perations_it}"
   }
 
   boot_disk {
